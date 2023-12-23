95b44f2455d581fa8162599a773f1140fd701315
ys <shuaiyue11@gmail.com>
2023-12-23 23:57:18 +0800

    调到fft_corr了

--- include/utils.h
+++ include/utils.h
@@ -1,30 +1,32 @@
-#ifndef HAVE_UTILS
-#define HAVE_UTILS
-
 #include <vector>
 #include <itpp/itbase.h>
 
-itpp::cvec raw2iq(const std::vector<int8_t> &raw);
+#ifndef HAVE_UTILS
+#define HAVE_UTILS
+
+
+extern itpp::cvec raw2iq(const std::vector<int8_t> &raw);
 
 // 定义 sinc 函数，模拟 MATLAB 中的 sinc 函数
-double sinc(double x);
+extern double sinc(double x);
 
-itpp::cvec zadoff_chu(int l, int i);
+extern itpp::cvec zadoff_chu(int l, int i);
 
-itpp::cvec pss(int n_id_2);
+extern itpp::cvec pss(int n_id_2);
 
-void pss_gen(itpp::cmat &td_pss);
+extern void pss_gen(itpp::cmat &td_pss);
 
-itpp::cmat pss_fo_set_gen(itpp::cmat &pss, const itpp::vec &fo_search_set);
+extern itpp::cmat pss_fo_set_gen(itpp::cmat &pss, const itpp::vec &fo_search_set);
 
-itpp::cvec shift(const itpp::cvec &vec, int len);
+extern itpp::cvec shift(const itpp::cvec &vec, int len);
 
-itpp::mat fft_corr(itpp::cvec s, itpp::cmat &td_pss, const itpp::vec &fo_search_set);
+extern itpp::mat fft_corr(itpp::cvec s, itpp::cmat &td_pss, const itpp::vec &fo_search_set);
 
-itpp::vec absx2(const itpp::cvec &capbuf);
+extern itpp::vec absx2(const itpp::cvec &capbuf);
 
 // mat reshape(vec &sp, int m, int n);
 
-itpp::vec sum_by_columns(itpp::mat matrix);
+extern itpp::vec sum_by_columns(itpp::mat matrix);
 
+extern void tshiftmy(itpp::vec &x, int t);
 #endif
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,3 +1,5 @@
+
+
 #include <itpp/itbase.h>
 #include <itpp/base/vec.h>
 #include <itpp/signal/transforms.h>
@@ -16,6 +18,7 @@
 #include "constants.h"
 #include "itpp_ext.h"
 #include "dsp.h"
+#include "utils.h"
 
 using namespace itpp;
 using namespace std;
@@ -43,18 +46,13 @@ cvec zadoff_chu(int l, int i)
 {
     cvec y(l);
     if (l & 1)
-    { // 如果 l 是奇数
-        for (int n = 0; n < l; ++n)
-        {
-            y[n] = std::exp(-std::complex<double>(0, 1) * i * M_PI / l * n * (1 + n));
-        }
+    {
+        y = itpp::exp(elem_mult(-std::complex<double>(0, 1) * i * M_PI / l * linspace(0, l - 1, l), to_cvec(linspace(1, l, l))));
     }
     else
     {
-        for (int n = 0; n < l; ++n)
-        {
-            y[n] = std::exp(-std::complex<double>(0, 1) * i * M_PI / l * n * n);
-        }
+        cvec t = -std::complex<double>(0, 1) * i * M_PI / l * linspace(0, l - 1, l);
+        y = itpp::exp(elem_mult(t, t));
     }
     return y;
 }
@@ -62,22 +60,22 @@ cvec zadoff_chu(int l, int i)
 cvec pss(int n_id_2)
 {
     int lut[] = {25, 29, 34};
-    cvec y = zadoff_chu(63, lut[n_id_2 + 1]);
-    y.del(32);
+    cvec y = zadoff_chu(63, lut[n_id_2]);
+    y.del(31);
     return y;
 }
 
 void pss_gen(cmat &td_pss)
 {
     cmat fd_pss(128, 3);
-    // cmat td_pss(128 + 9, 3);
     for (size_t i = 0; i < 3; i++)
     {
         cvec temp = pss(i); // 假设 pss 函数返回 vec 类型
-        cvec t = to_cvec(vec(0), vec(0));
-        fd_pss.set_col(i, concat(t, temp.get(31, 128 - 1), zeros_c(65), temp.get(0, 30)));
-        cvec temp_td = ifft(fd_pss.get_col(i)) * std::sqrt(128.0 / 62.0);
-        td_pss.set_col(i, concat(temp_td.get(128 - 8, 128 - 1), temp_td.get(0, 128 - 9)));
+        cvec t = zeros_c(1);
+        cvec row = concat(t, temp.get(31, -1), zeros_c(65), temp.get(0, 30));
+        fd_pss.set_col(i, row);
+        cvec temp_td = idft(fd_pss.get_col(i)) * std::sqrt(128.0 / 62.0);
+        td_pss.set_col(i, concat(temp_td.get(128 - 9, 128 - 1), temp_td));
     }
 }
 
@@ -91,15 +89,17 @@ cmat pss_fo_set_gen(cmat &pss, const itpp::vec &fo_search_set)
 
     cmat pss_fo_set = zeros_c(len_pss, num_fo * num_pss);
 
+    cvec tmp = cvec(len_pss);
+    for (size_t i = 0; i < len_pss; i++)
+    {
+        tmp[i] = complex<double>(0, 2 * M_PI * (1.0 / sampling_rate) * i);
+    }
+
     for (size_t i = 0; i < num_pss; i++)
     {
         size_t sp = i * num_fo;
         size_t ep = sp + num_fo - 1;
-        for (size_t col = sp; col <= ep; col++)
-        {
-            cmat tmp = kron(ones_c(1, num_fo), pss(0, pss.rows() - 1, i, i)) * itpp::exp(std::complex<double>(0, 1) * 2 * M_PI * (1.0 / sampling_rate) * itpp::linspace(0, len_pss - 1, len_pss) * fo_search_set);
-            pss_fo_set.set_submatrix(0, sp, conj(tmp) / len_pss);
-        }
+        pss_fo_set.set_submatrix(0, sp, conj(elem_mult(kron(ones_c(1, num_fo), cmat(pss.get_col(i))), exp(outer_product(tmp, to_cvec(fo_search_set))))) / len_pss);
     }
     return pss_fo_set;
 }
@@ -121,25 +121,27 @@ mat fft_corr(cvec s, cmat &td_pss, const vec &fo_search_set)
     size_t num_fo = fo_search_set.size();
     size_t num_fo_pss = num_fo * num_pss;
     mat corr_store(len, num_fo_pss);
-
     s = fft(s);
-    cmat fd_pss(len_pss, num_fo_pss);
-    for (int i = 0; i < num_fo_pss; ++i)
+    cmat fd_pss(len, num_pss);
+    for (int i = 0; i < num_pss; ++i)
     {
-        fd_pss.set_col(i, itpp::fft(itpp::conj(itpp::reverse(td_pss.get_row(i)) / (int)len_pss), len_pss));
+        fd_pss.set_col(i, itpp::fft(itpp::conj(itpp::reverse(td_pss.get_col(i)) / (int)len_pss), len));
     }
+    cout << "fd_pss" << fd_pss(0, 0) << fd_pss(0, 1) << fd_pss(1, 0) << fd_pss(1, 1) << endl;
+
     for (int i = 0; i < num_fo_pss; ++i)
     {
-        int pss_idx = (i / num_fo) + 1;
-        int fo_idx = i - (pss_idx - 1) * num_fo;
+        int pss_idx = floor(i / num_fo);
+        int fo_idx = i - pss_idx * num_fo;
         double fo = fo_search_set(fo_idx);
         double fd_fo_shift_len = fo / freq_step;
 
         // Perform circular shift and element-wise multiplication
-        cvec tmp = s.transpose() * shift(fd_pss.get_col(pss_idx - 1), fd_fo_shift_len);
-        corr_store.set_col(i, sqrt(itpp::abs(ifft(tmp))));
+        vec tmp = abs(ifft(elem_mult(s, shift(fd_pss.get_col(pss_idx), fd_fo_shift_len)), len));
+        corr_store.set_col(i, elem_mult(tmp, tmp));
     }
-    corr_store = corr_store(len_pss, corr_store.rows() - 1, 0, corr_store.cols() - 1);
+    corr_store = corr_store(len_pss, -1, 0, -1);
+    cout << "corr_store" << corr_store(0, 0) << corr_store(0, 1) << corr_store(1, 0) << corr_store(1, 1) << endl;
     return corr_store;
 }
 
@@ -166,4 +168,24 @@ vec sum_by_columns(mat matrix)
         result[i] = sum(matrix.get_col(i));
     }
     return result;
+}
+
+void tshiftmy(vec &x, int t)
+{
+    vec y(x);
+    if (t == floor(t))
+    {
+        t = mod(t, length(x));
+        for (int i = 0; i < length(x) - t; ++i)
+        {
+            y[t + i] = x[i];
+        }
+
+        // Second assignment: y(1:t) = x(end-t+1:end);
+        for (int i = 0; i < t; ++i)
+        {
+            y[i] = x[length(x) - t + i];
+        }
+        return;
+    }
 }
